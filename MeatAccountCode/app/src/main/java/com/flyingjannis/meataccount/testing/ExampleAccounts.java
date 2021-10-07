d4ba1da98b1a1566701a4295dc860ff9e5f0cf8b
FlyingJannis <schramek@students.uni-marburg.de>
2021-10-07 13:38:27 +0200

    getRandomAccountV2()

--- MeatAccountCode/app/src/main/java/com/flyingjannis/meataccount/testing/ExampleAccounts.java
+++ MeatAccountCode/app/src/main/java/com/flyingjannis/meataccount/testing/ExampleAccounts.java
@@ -1,7 +1,9 @@
 package com.flyingjannis.meataccount.testing;
 
 import com.flyingjannis.meataccount.model.Account;
+import com.flyingjannis.meataccount.model.AccountV2;
 import com.flyingjannis.meataccount.model.WeekStamp;
+import com.flyingjannis.meataccount.model.WeekStampV2;
 
 import java.util.Calendar;
 import java.util.Random;
@@ -25,6 +27,27 @@ public class ExampleAccounts {
             account.getWeeks()[i] = tmp;
         }
         account.getWeeks()[weeks] = new WeekStamp(weeks);
+        account.setPayments(weeks);
+        return account;
+    }
+
+    public static AccountV2 getRandomAccountV2(int weeks, int amount) {
+        AccountV2 account = new AccountV2(amount);
+        Calendar calender = Calendar.getInstance();
+        account.setCreationDateMillis(calender.getTimeInMillis() - ((long) weeks * 604800000) -
+                (calender.get(Calendar.MINUTE) * 60000) - (calender.get(Calendar.SECOND) * 1000));
+        account.setWeeks(new WeekStampV2[weeks + 1]);
+        for(int i = 0; i < account.getWeeks().length - 1; i++) {
+            WeekStampV2 tmp = new WeekStampV2(i);
+            Random random = new Random();
+            for(int j = 0; j < 8; j++) {
+                int x = random.nextInt(200);
+                if(x < 160) x = 0;
+                tmp.setDay(j, x);
+            }
+            account.getWeeks()[i] = tmp;
+        }
+        account.getWeeks()[weeks] = new WeekStampV2(weeks);
         account.setPayments(weeks);
         return account;
     }
